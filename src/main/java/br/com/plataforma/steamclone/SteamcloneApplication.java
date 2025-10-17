a39edaf7cca86795506acdc5554222d389201b2f
Victor de Amorim Rodrigues <euvictoramorimm@users.noreply.github.com>
2025-10-16 23:32:42 -0300

    Commit inicial: Configuração do projeto Spring Boot e H2

---
+++ src/main/java/br/com/plataforma/steamclone/SteamcloneApplication.java
@@ -0,0 +1,13 @@
+package br.com.plataforma.steamclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamcloneApplication.class, args);
+	}
+
+}
